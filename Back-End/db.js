5ed26f51c7192d744fe00b5e4acb9680c305d0b8
Noor Alfaqeeh <noor.alfaqqeh#hotmail.com>
2019-10-19 16:15:56 +0300

    ToDo-List

--- Back-End/db.js
+++ Back-End/db.js
@@ -16,14 +16,17 @@ let tasksSchema = new mongoose.Schema({
   isCompleted: Boolean
 });
 let Tasks = mongoose.model('tasks', tasksSchema);
+
 let getTasks = cb => {
   console.log('GET TASKS FROM DATABASE');
-  Tasks.find({}, function(err, docs) {
+  Tasks.find({},function(err, docs) {
     if (err) {
       console.log('ERR:', err);
+      cb(err);
+    }else{
+      console.log('DOCS:', docs);
+      cb(docs);
     }
-    console.log('DOCS:', docs);
-    cb(docs);
   });
 };
 
@@ -42,11 +45,55 @@ let insertTask = (cb, obj) => {
   });
 };
 
-let removeOne = (cb, ID) => {
-  cb('DATABASE AFTER REMOVE');
+let removeOne = (cb, _ID) => {
+  // cb('DATABASE AFTER REMOVE');
+  Tasks.findByIdAndRemove(_ID, (err,docs)=> {
+    console.log('object_ID from DB :', _ID);
+    if (err) {
+      cb(err)
+      console.log('ERR:', err);
+    }else{ 
+       getTasks(cb);
+       console.log('DOCS:', docs);
+    }
+    
+  });
 };
+
+
+let editTask = (_id,isCompleted,cb) => {
+console.log('id from DB-----------------',_id),
+console.log('isCompleted from DB-----------------', isCompleted)
+isCompleted = (isCompleted == 'true');
+
+  Tasks.findByIdAndUpdate(_id, {$set:{isCompleted: !isCompleted}},(err,docs)=>{
+    if (err){
+      cb(err)
+    }else{
+      getTasks(cb);
+    }
+  })
+}
+
+
+// Tasks.findByIdAndUpdate(togPrivate._id,
+//   { $set: { private: !togPrivate.private } },
+//   (err, data) => {
+//     if (err) {
+//       cb(err)
+//     } else {
+//       // console.log('new data:', data);
+//       readRepos(cb)
+//     }
+//   })
+
+
+
+
+
 module.exports = {
   abeer: getTasks,
   insert: insertTask,
-  remove: removeOne
+  remove: removeOne,
+  edit: editTask
 };
